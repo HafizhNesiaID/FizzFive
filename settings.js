cdd3b8a51db69f6dbb2fbbdb454a866fa74d2396
HafizhNesiaID <149253106+HafizhNesiaID@users.noreply.github.com>
2024-05-17 11:52:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,10 +31,10 @@ keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_dat
 
 //note service SMTP Gmail Tidak Boleh Send Kat Temp Email Kalau mahu pakai temp email buat di service lain
 usetempemail = false // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
-servicesmtp = 'Gmail'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
-sendemail = process.env.sendemail //Email SMTP
-fromsendemail = process.env.fromsendemail // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
-sendpwmail = process.env.sendpwmail // Password Email SMTP
+servicesmtp = 'JagoanHosting'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
+sendemail = 'support@fiverly-pedia.my.id' //Email SMTP
+fromsendemail = 'support@fiverly-pedia.my.id' // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
+sendpwmail = '@@Server@@' // Password Email SMTP
 domain = 'aps.navaweb.my.id'// domain website tanpa http or https
 
 //_______________________ ┏  Apikey  ┓ _______________________\\
