992a01fa62bc0441af3616ca74fa28d5651c9e14
HafizhNesiaID <149253106+HafizhNesiaID@users.noreply.github.com>
2024-05-17 12:03:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ LimitApikey = 200 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
-keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
+keymongodb = 'mongodb+srv://fizzdontcare:@@Server@@@apsnw.nnlixzb.mongodb.net/?retryWrites=true&w=majority&appName=APSNW' //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
 
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
